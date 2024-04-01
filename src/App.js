59612f9793ad08508bf0cdbbcb12536c1552c6ed
Leonardo Caceres <leomoca19@gmail.com>
2024-04-01 03:59:51 +0000

    finish game, game works now

--- src/App.js
+++ src/App.js
@@ -7,16 +7,28 @@ const Square = ({ val, onSquareClick }) => (
 );
 
 const Board = () => {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   const handleClick = (i) => {
+    if (squares[i] || calculateWinner(squares)) return;
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
+
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   };
+
+  const winner = calculateWinner(squares);
+  let status;
+  winner
+    ? (status = `Winner: ${winner}`)
+    : (status = `Next player: ${xIsNext ? "X" : "O"}`);
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square val={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square val={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -34,6 +46,27 @@ const Board = () => {
       </div>
     </>
   );
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+      return squares[a];
+  }
+
+  return null;
 };
 
 export default Board;
